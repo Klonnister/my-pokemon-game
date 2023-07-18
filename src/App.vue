744f9b2c8d62a61962a2a21b680d1bb9ff1371c7
Dennis Herrera <dennishumher25@hotmail.com>
2023-07-18 12:39:50 -0600

    moved App content to PokemonPage and imported it

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,19 @@
 <template>
-  <main>
-  <h1 v-if="pokemon">¿Cuál es este Pokémon?</h1>
-  <p  v-if="!pokemon" class="waiting">Espere, por favor...</p>
-
-  <div v-if="playing">
-    <PokemonImage 
-      v-if="this.pokemon"
-      :pokemon-id="this.pokemon.id"
-      :show-images="showImages"
-      class="fade-in"
-    />
-
-    <PokemonOptions 
-      :pokemons="pokemonArr"
-      @selection="checkAnswer"
-      :pokemon="pokemon"
-    />
-
-    <div v-if="showMessage" class="fade-in">
-      <h2> {{ message }} </h2>
-      <button      
-        class="btn"
-        @click="nextRound"
-      >
-      Siguiente
-      </button>
-    </div>
-  </div>
-
-  <button
-    v-if="!playing"
-    type="button"
-    class="btn"
-    @click="newGame"
-  >
-    Empezar
-  </button>
-
-  </main>
+  
+  <PokemonPage />
+  
 </template>
 
 <script>
-import PokemonImage from './components/PokemonImage.vue'
-import PokemonOptions from './components/PokemonOptions.vue'
-
-import pokemonOptions from '@/helpers/getPokemonOptions'
-
+import PokemonPage from './pages/PokemonPage.vue'
 
 export default {
   name: 'App',
   components: {
-    PokemonImage,
-    PokemonOptions,
-  },
-
-  data() {
-    return {
-      showImages: false,
-      pokemonArr: [],
-      pokemon: null,
-      message: null,
-      showMessage: false,
-      playing: false
-    }
-  },
-
-  methods: {
-    async getRandomPokemon() {
-      this.pokemonArr = await pokemonOptions()
-
-      const rndInt = Math.floor(Math.random() * 4)
-      
-      this.pokemon = this.pokemonArr[ rndInt ]
-    },
-
-    checkAnswer( pokemonId ) {
-      this.showImages = true
-
-      if( this.pokemon.id === pokemonId ) {
-        this.message = `Correcto, era ${ this.pokemon.name }!`
-      } else {
-        this.message = `Oops, era ${ this.pokemon.name }`
-      }
-
-      this.showMessage = true
-    },
-
-    nextRound() {
-      this.showImages = false
-      this.pokemonArr = []
-      this.pokemon =  null
-      this.message = null
-      this.showMessage = false
-
-      this.getRandomPokemon()
-    },
-
-    newGame() {
-      this.playing = true
-    },
-  },
-
-  mounted() {
-      this.getRandomPokemon()
-    }
+    PokemonPage
+  }
 }
+
 </script>
 
 <style>
@@ -114,28 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   text-align: center;
-}
-
-main {
-  background-color: hsla(0, 0%, 86%, 75%);
-  padding: 1rem 1rem;
-  margin: 0 1rem;
-  border-radius: 1.5rem;
-
-  @media screen and ( min-width: 33.75rem ) {
-    padding: 1rem 4rem;
-  }
-}
-
-.btn {
-  padding: 0.5rem 1rem;
-  background-color: #2c3e50;
-  color: white;
-  border-radius: 1rem;
-}
-
-.waiting {
-  font-size: 2rem;
-  font-weight: bold;
 }
 </style>
