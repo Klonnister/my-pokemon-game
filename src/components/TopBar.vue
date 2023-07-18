e80e286feeb17b0449143493b457d54f149d0574
Dennis Herrera <dennishumher25@hotmail.com>
2023-07-18 13:08:17 -0600

    added topBar as component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="top-bar">
+    <p>Punteo: {{ points }}</p>
+    <p>Intentos: {{ intentos }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        points: {
+            type: Number,
+            required: true
+        },
+        intentos: {
+            type: Number,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.top-bar {
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  inset: 0;
+  top: 0;
+  margin: 0 auto;
+  width: 90%;
+  height: 5%;
+  z-index: 0;
+  font-size: 0.75rem;
+}
+</style>
